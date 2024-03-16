d8c610a86e3f2437181b47bf957f484451e9c6ba
telmo-checa <telmocheca1@gmail.com>
2024-03-16 19:49:24 +0100

    Ultimato il jsnack1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 console.log("JS SNACKS");
+
+//JSnack 1
+//L’utente inserisce due numeri in successione, con due prompt.
+//--Il software stampa il maggiore.
+
+//Chiedere due numeri al utente
+const primoNum = prompt("Scelie il primo numero");
+const secondoNum = prompt("Sceglie il secondo numero");
+console.log( "Primo numero " + primoNum, "Secondo numero " +secondoNum);
+
+  //Controlo tra i due numeri quale è il maggiore
+  //--SE il primo numero è maggiore
+    if(primoNum > secondoNum){
+      //---Stampa il primo numero come maggiore
+      console.log("Il numero magiore è: " + primoNum);
+      
+      //--ALTRIMENTI SE il primo numero è minore
+    }else if(primoNum < secondoNum){
+      //---Stampa il secondo numero come maggiore
+      console.log("Il numero magiore è: " + secondoNum);
+      
+      //--ALTRIMENTI SE i numeri sono uguali
+    }  else if(primoNum === secondoNum){
+      //---Stampa numeri uguali
+      console.log("I numeri sono uguali");
+      
+      //--ALTRIMENTI
+    }else{
+      console("Scelta non valida");
+    }
+
+    
